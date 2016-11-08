207611381ddfa4bed5a0b92f7c055759d5762ec4
Hao Wang <wang.hao.chnbj@gmail.com>
2016-11-08 14:19:38 -0500

    updated the pod source

--- AppFriendsChatSample/AppFriendsChatSample/ViewController.swift
+++ AppFriendsChatSample/AppFriendsChatSample/ViewController.swift
@@ -28,7 +28,7 @@ class ViewController: UIViewController {
                 }
             }
             else {
-                print(error?.localizedDescription)
+                print(error?.localizedDescription ?? "")
             }
         }
     }
