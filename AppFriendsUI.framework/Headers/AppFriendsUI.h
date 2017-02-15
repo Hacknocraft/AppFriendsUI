889918faf6039e1d6e1d00afd7cf59c036c05a92
wanghao <hao.wang@hacknocraft.com>
2017-02-14 23:50:32 -0500

    commit release 1.2.14

---
+++ AppFriendsUI.framework/Headers/AppFriendsUI.h
@@ -0,0 +1,19 @@
+//
+//  AppFriendsUI.h
+//  AppFriendsUI
+//
+//  Created by HAO WANG on 11/5/16.
+//  Copyright © 2016 hacknocraft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppFriendsUI.
+FOUNDATION_EXPORT double AppFriendsUIVersionNumber;
+
+//! Project version string for AppFriendsUI.
+FOUNDATION_EXPORT const unsigned char AppFriendsUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppFriendsUI/PublicHeader.h>
+
+
