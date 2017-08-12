37902159a685518041fc064b8a4a501f30f18344
wanghao <hao.wang@hacknocraft.com>
2017-08-12 12:59:16 -0400

    commit release 2.1.2

--- AppFriendsUI.framework/Headers/AppFriendsUI-Swift.h
+++ AppFriendsUI.framework/Headers/AppFriendsUI-Swift.h
@@ -607,7 +607,7 @@ SWIFT_CLASS("_TtC12AppFriendsUI6AFUser")
 /// \param completion completion block. Will report error if the call fails
 ///
 + (void)updateUserAvatarWithAvatar:(NSString * _Nonnull)avatar completion:(void (^ _Nullable)(NSError * _Nullable))completion;
-+ (void)updateUserWithUsername:(NSString * _Nonnull)username avatar:(NSString * _Nonnull)avatar completion:(void (^ _Nullable)(NSError * _Nullable))completion;
++ (void)updateUserWithUsername:(NSString * _Nullable)username avatar:(NSString * _Nullable)avatar completion:(void (^ _Nullable)(NSError * _Nullable))completion;
 /// update custom data of the current user
 /// \param custom data new custom data
 ///
